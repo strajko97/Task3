7d8a15165a13eafa1b47e517460fbbad76d62146
Strahinja Paunkovic <paunkovicwork@gmail.com>
2022-07-28 12:26:53 +0200

    added line to show conflict in git

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -51,7 +51,7 @@ namespace Data
             modelBuilder.Entity<Country>().HasData(countries);
 
 
-
+            // izmena na liniji 54
             LinkedList<Sms> smss = new LinkedList<Sms>();
             Sms sms1 = new Sms { SendTime = DateTime.Now, CountryId = 262, From = "straja", To = "tea", SmsId = 1, Text = "dsadsa", Status=1 };
             Sms sms2 = new Sms { SendTime = DateTime.Now, CountryId = 232, From = "straja", To = "tea", SmsId = 2, Text = "dahhahaha", Status = 0 };
